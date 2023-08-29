35858ee4c2566f98385008679278cf58a93efe69
Priyanshu Trivedi <priyanshutrivedi0081@gmail.com>
2023-08-29 11:08:44 +0530

    added the logic of delete button

--- main.js
+++ main.js
@@ -11,11 +11,18 @@ for (let i = 0; i < buttons.length; i++) {
             string = "";
             document.querySelector('input').value = string;
         }
-        else {
-            console.log(e.target);
-            string = string + e.target.innerHTML;
-            console.log(string);
+        else if (e.target.innerHTML == '<ion-icon name="backspace-outline" role="img" class="md hydrated"></ion-icon>' || e.target.innerHTML == '') {
+            string = string.toString();
+            string = string.substring(0,string.length - 1);
             document.querySelector('input').value = string;
         }
+        else {
+            // console.log(e.target);
+            string = string + e.target.innerHTML;
+            // console.log(string);
+            document.querySelector('input').value = string;
+        }
+        // console.log(e.target.innerHTML);
+        // console.log('string = ' + string);
     })
 }
